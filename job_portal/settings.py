c2df0bb7a7b66debb5f1f970d5756d58c5fb1e81
akhil4735 <dartsandreconsofsova@gmail.com>
2025-08-10 22:41:23 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -138,4 +138,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['jobconnect-bpqr.onrender.com']
